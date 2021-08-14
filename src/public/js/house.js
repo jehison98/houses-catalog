16f4c209db4aa64db33cf318a57d32e5f3d07bc9
Jehison Gutierrez <jehison-98@hotmail.com>
2021-08-13 21:49:15 -0500

    Replace the imgs to external server(drive), add touch move in gallery details

--- src/public/js/house.js
+++ src/public/js/house.js
@@ -75,6 +75,55 @@ window.onkeyup = (e) => {
 arrowright.addEventListener('click', nextImg);
 arrowLeft.addEventListener('click', prevImg);
 
+
+slider.addEventListener('touchstart', handleTouchStart, false);        
+slider.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+            nextImg();
+        } else {
+            /* left swipe */
+            prevImg();
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+        } else { 
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
 //vars to count the elements are overflow on X
 let imagesCounter;
 //this count if the user click on next or prev
